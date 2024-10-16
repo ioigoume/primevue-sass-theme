0de5c5f944afacca521d04bbd48701b1e4312e6e
Ioannis Igoumenos <ioigoume@gmail.com>
2024-10-16 13:59:51 +0300

    add css compilation support

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const cssnano = require('cssnano')
+const postcssPresetEnv = require('postcss-preset-env')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+    plugins: [
+        cssnano({
+            preset: 'default',
+        }),
+        postcssPresetEnv({stage: 1}),
+    ],
+};
